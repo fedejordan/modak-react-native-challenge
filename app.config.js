4af258d62d58669f4113b1e0f0c892302d9855da
Federico Jordan <fedejordan99@gmail.com>
2025-08-18 01:41:11 -0300

    created project and configured eslint and prettier

---
+++ app.config.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-undef */
+
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: 'modak-rn-challenge',
+    slug: 'modak-rn-challenge',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    newArchEnabled: true,
+    splash: {
+      image: './assets/splash-icon.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+      edgeToEdgeEnabled: true,
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+    extra: {
+      apiBaseURL: process.env.API_BASE_URL,
+    },
+  },
+};
---
+++ eslint.config.cjs
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-require-imports, no-undef */
+const js = require('@eslint/js');
+const ts = require('typescript-eslint');
+const react = require('eslint-plugin-react');
+const reactHooks = require('eslint-plugin-react-hooks');
+const rn = require('eslint-plugin-react-native');
+const importPlugin = require('eslint-plugin-import');
+const prettier = require('eslint-config-prettier');
+const prettierPlugin = require('eslint-plugin-prettier');
+
+module.exports = [
+  // Ignorados
+  { ignores: ['node_modules/', 'dist/', 'build/', 'android/', 'ios/'] },
+
+  // ⬅️ Bloque GLOBAL con "settings" (aplica a todo)
+  {
+    settings: {
+      react: { version: 'detect' },
+      'import/resolver': { typescript: true, node: true },
+    },
+  },
+
+  // Presets
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  react.configs.flat.recommended,
+
+  // Reglas del proyecto
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    plugins: {
+      prettier: prettierPlugin,
+      'react-hooks': reactHooks,
+      'react-native': rn,
+      import: importPlugin,
+    },
+    rules: {
+      ...prettier.rules,
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+    },
+  },
+];
