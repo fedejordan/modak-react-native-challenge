41c45fd7cb3d9d7c5c4c66475abd7030b927a939
Federico Jordan <fedejordan99@gmail.com>
2025-08-20 03:09:02 -0300

    added restriction and alert for unavailable feature in android

--- src/components/AddReminderButton.tsx
+++ src/components/AddReminderButton.tsx
@@ -1,4 +1,4 @@
-import { Alert, Button } from 'react-native';
+import { Alert, Button, Platform } from 'react-native';
 import { addPurchaseReminder } from '@/native/purchaseReminder';
 
 function AddReminderButton({
@@ -12,12 +12,16 @@ function AddReminderButton({
 }) {
   const onPress = async () => {
     try {
-      await addPurchaseReminder(title, {
-        notes,
-        isoDate: whenISO,
-        durationMinutes: 60, // opcional
-      });
-      Alert.alert('Done ✅', 'Event created in calendar.');
+      if (Platform.OS === 'ios') {
+        await addPurchaseReminder(title, {
+          notes,
+          isoDate: whenISO,
+          durationMinutes: 60, // opcional
+        });
+        Alert.alert('Done ✅', 'Event created in calendar.');
+      } else {
+        Alert.alert('Info', 'This feature is only available on iOS.');
+      }
     } catch (e: any) {
       Alert.alert('Error', e?.message ?? "Event couldn't be created.");
     }
